2854135d71d64032601ab0d11171f363c07c8ccb
Joakim Johansson <johansson.joakim@gmail.com>
2014-07-24 23:32:27 +0200

    added countdown

--- app/models/story.rb
+++ app/models/story.rb
@@ -5,4 +5,15 @@ class Story < ActiveRecord::Base
     validates :title, presence: true, length: { maximum: 25 }
     validates :content, presence: true, length: { maximum: 2000 }
     validates :user_id, presence: true
+
+    def timeLeft
+        if (self.created_at > 16.hour.ago)
+            timeLeft = ((Time.parse(DateTime.now.to_s) - Time.parse(self.created_at.to_s))/3600) - ((Time.parse(DateTime.now.to_s) - Time.parse(self.created_at.to_s))/3600).floor
+            timeLeft = (60 - (60 * timeLeft)).ceil
+            timeLeft = Time.now + timeLeft.minutes
+            timeLeft = timeLeft.strftime("%Y/%m/%d %H:%M:%S")
+        else 
+            timeLeft = false
+        end
+    end
 end
