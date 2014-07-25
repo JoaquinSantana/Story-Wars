85d59285516a0f098d9bfcc6a041658a544894e0
Joakim Johansson <johansson.joakim@gmail.com>
2014-07-25 16:33:27 +0200

    debuging countdown

--- app/assets/javascripts/initCountdown.js
+++ app/assets/javascripts/initCountdown.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
     var clock = $('#clock');
     // init jquery.countdown
+    console.log("clock");
     clock.countdown(clock.data("time")).on('update.countdown', function(event) {
+        console.log("countdown");
         var $this = $(this).html(event.strftime('<span>%M</span> min'));
     }); 
 });
