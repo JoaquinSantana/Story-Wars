30880c8a88de5faa8081e63c3fb760bf3a3c8bc2
Joakim Johansson <johansson.joakim@gmail.com>
2014-08-01 22:14:03 +0200

    fixed footer

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -1,5 +1,6 @@
 class Chapter < ActiveRecord::Base
     belongs_to :story
+    belongs_to :chapter
     has_many :votes, dependent: :destroy
     has_many :upvoted_users, through: :votes, source: :user
 end
