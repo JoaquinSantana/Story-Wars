29ce865963bc8dd0cf4ae87b4b1be231b2de3168
Joakim Johansson <johansson.joakim@gmail.com>
2014-08-03 14:12:44 +0200

    fixed iOS icons

--- app/assets/javascripts/initReadmore.js
+++ app/assets/javascripts/initReadmore.js
@@ -1,7 +1,8 @@
 $(document).on('ready page:load', function() {
-    $('article.voteChapter-content').readmore({
+    article = $('article.voteChapter-content');
+    article.readmore({
         speed: 150,
         maxHeight: 102,
-        moreLink: '<a href="#">Read more and vote</a>'
+        moreLink: '<a href="#">' + article.data('readmore') + '</a>'
     });
 });
