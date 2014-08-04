8db9f81901fc3be7786a2fcf33ac8f7acf925241
Joakim Johansson <johansson.joakim@gmail.com>
2014-08-04 09:10:59 +0200

    added challenge friends on every story

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
     has_many :stories, dependent: :destroy
     before_save { self.email = email.downcase }
     before_create :create_remember_token
-    validates :name, presence: true, length: { maximum: 50 }
+    validates :name, presence: true, length: { maximum: 50 }, uniqueness: { case_sensitive: true }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
     has_secure_password
